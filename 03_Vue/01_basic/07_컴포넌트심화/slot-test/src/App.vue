fd3f3b2ad81c2e279d4ba39b212ad90c3c761972
hyevin <vveny113@gmail.com>
2026-03-25 16:49:38 +0900

    fix: 20260325_추가
    fix: 20260325_추가

---
+++ 03_Vue/01_basic/07_컴포넌트심화/slot-test/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import SlotTest from "@/components/example1/SlotTest.vue";
+
+
+</script>
+
+<template>
+  <!--  <NoSlotTest/>-->
+  <SlotTest/>
+</template>
+
+<style scoped></style>
---
+++ 03_Vue/01_basic/07_컴포넌트심화/style-test/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import ScopedCssDemo from "@/components/ScopedCssDemo.vue";
+import GlobalCssDemo from "@/components/GlobalCssDemo.vue";
+import CssModuleDemo from "@/components/CssModuleDemo.vue";
+</script>
+
+<template>
+  <h1>Vue Css 스타일 방법</h1>
+  <!-- 전역 CSS-->
+  <GlobalCssDemo/>
+  <!-- scoped CSS-->
+  <ScopedCssDemo/>
+  <!-- CSS Module -->
+  <CssModuleDemo/>
+</template>
+
+<!-- 전역 -->
+<!-- App.vue의 <style>: 전역처럼 동작 -->
+<!-- main.css와 역할을 구분하기 위해 주로 앱의 레이아웃구성 -->
+<style>
+.container {
+  max-width: 600px;
+  margin: 40px auto;
+}
+</style>
